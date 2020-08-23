e80b3de03539a98c9d09ab089e5f6f27f4f84100
liuhongdi <liuhongdi@gmail.com>
2020-08-23 20:23:36 +0800

    add session display

--- src/main/java/com/actuator/demo/controller/HomeController.java
+++ src/main/java/com/actuator/demo/controller/HomeController.java
@@ -2,9 +2,14 @@ package com.actuator.demo.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.context.request.RequestContextHolder;
+import org.springframework.web.context.request.ServletRequestAttributes;
 
 import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
+import java.util.Enumeration;
 
 @RestController
 @RequestMapping("/home")
@@ -14,5 +19,20 @@ public class HomeController {
     public String ip(HttpServletRequest request) {
        String ip = request.getRemoteAddr();
        return ip;
+    }
+
+    //session详情
+    @GetMapping("/session")
+    @ResponseBody
+    public String session() {
+        HttpSession session = ((ServletRequestAttributes) RequestContextHolder.getRequestAttributes()).getRequest().getSession();
+        Enumeration e   =   session.getAttributeNames();
+        String s = "";
+        while( e.hasMoreElements())   {
+            String sessionName=(String)e.nextElement();
+            s += "name="+sessionName+";<br/>";
+            s += "value="+session.getAttribute(sessionName)+";";
+        }
+        return s;
     }
 }
--- src/main/java/com/actuator/demo/security/SecurityConfig.java
+++ src/main/java/com/actuator/demo/security/SecurityConfig.java
@@ -14,59 +14,56 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 
 @Configuration
 @EnableWebSecurity
-//@EnableGlobalMethodSecurity(prePostEnabled = true)  //  启用方法级别的权限认证
  public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
 
      @Value("${management.access.iplist}")
      private String iplist;
 
-     @Override
-     protected void configure(HttpSecurity http) throws Exception {
-                 /*
-                 //  允许所有用户访问"/"和"/index.html"
-                 http.authorizeRequests()
-                         .antMatchers("/", "/index.html").permitAll()
-                         .anyRequest().authenticated()   // 其他地址的访问均需验证权限
-                         .and()
-                         .formLogin()
-                         .loginPage("/login.html")   //  登录页
-                         .failureUrl("/login-error.html").permitAll()
-                         .and()
-                         .logout()
-                         .logoutSuccessUrl("/index.html");
-                 */
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        //得到iplist列表
+        String iprule = "";
+        //hasIpAddress('10.0.0.0/16') or hasIpAddress('127.0.0.1/32')
+        String[] splitAddress=iplist.split(",");
+        for(String ip : splitAddress){
+            if (iprule.equals("")) {
+                iprule = "hasIpAddress('"+ip+"')";
+            } else {
+                iprule += " or hasIpAddress('"+ip+"')";
+            }
+        }
+        String actuatorRule = "hasAnyRole('ADMIN','DEV') and ("+iprule+")";
 
-                //System.out.println(iplist);
-                 //得到iplist列表
-                String iprule = "";
-                //hasIpAddress('10.0.0.0/16') or hasIpAddress('127.0.0.1/32')
-                String[] splitAddress=iplist.split(",");
-                for(String ip : splitAddress){
-                     //System.out.println(ip);
-                     if (iprule.equals("")) {
-                         iprule = "hasIpAddress('"+ip+"')";
-                     } else {
-                         iprule += " or hasIpAddress('"+ip+"')";
-                     }
-                }
-               System.out.println(iprule);
+        //login和logout
+        http.formLogin()
+                .defaultSuccessUrl("/home/session")
+                .failureUrl("/login-error.html")
+                .permitAll()
+                .and()
+                .logout();
 
-         http.authorizeRequests()
-                         //.antMatchers("/actuator/**").hasIpAddress("127.0.0.1")
-                 //.antMatchers("/admin/**").access("hasRole('admin') and (hasIpAddress('127.0.0.1') or hasIpAddress('192.168.1.0/24') or hasIpAddress('0:0:0:0:0:0:0:1'))");
-                 .antMatchers("/lhdmon/**").access(iprule)
-                 .antMatchers("/home/**").hasIpAddress("192.168.1.6")
-                 .anyRequest().permitAll();
-             }
-             
-     @Autowired
-     public  void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
-         /*
-         auth.inMemoryAuthentication()
-                 .withUser("laoliu").password("lhddemo").roles("ADMIN")
-                 .and()
-                 .withUser("laoliu2").password("lhddemo").roles("ADMIN");
-         */
-     }
+        //匹配的页面，符合限制才可访问
+        http.authorizeRequests()
+                //.antMatchers("/actuator/**").hasIpAddress("127.0.0.1")
+                //.antMatchers("/admin/**").access("hasRole('admin') and (hasIpAddress('127.0.0.1') or hasIpAddress('192.168.1.0/24') or hasIpAddress('0:0:0:0:0:0:0:1'))");
+                .antMatchers("/lhdmon/**").access(actuatorRule)
+                .antMatchers("/goods/**").hasAnyRole("ADMIN","DEV");
+
+        //剩下的页面，允许访问
+        http.authorizeRequests().anyRequest().permitAll();
+    }
+
+    @Autowired
+    public  void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
+        //添加两个账号用来做测试
+        auth.inMemoryAuthentication().passwordEncoder(new BCryptPasswordEncoder())
+                .withUser("lhdadmin")
+                .password(new BCryptPasswordEncoder().encode("123456"))
+                .roles("ADMIN","USER")
+                .and()
+                .withUser("lhduser")
+                .password(new BCryptPasswordEncoder().encode("123456"))
+                .roles("USER");
+    }
  }
